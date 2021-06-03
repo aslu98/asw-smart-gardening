bedd53bd960f544dd1306274a12aed8f9080b366
Asia <asia.lucchi@studio.unibo.it>
2021-06-03 15:45:45 +0200

    deleted home from router

--- code/public/js/router.js
+++ code/public/js/router.js
@@ -1,7 +1,6 @@
 const router = new VueRouter({
     mode: 'history',
     routes: [
-      { path: '/', name:'Home', component: Home },
       { path: '/404', component: NotFound },  
       { path: '*', redirect: '/404' }
     ]
