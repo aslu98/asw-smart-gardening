7507e7f1312e91b6cc64d60340e15efcb9393117
Asia <asia.lucchi@studio.unibo.it>
2021-06-03 16:23:43 +0200

    solved row margin problem

--- code/public/js/gardener-board.js
+++ code/public/js/gardener-board.js
@@ -6,21 +6,19 @@ const GardenerBoard = {
 		'gardener-calendar': GardenerCalendar
 	},
 	template: `
-		<div class="gardener-board">
-			<div class="row"> 
-				<div class="col-12 col-md-7">
-					<gardener-calendar></gardener-calendar>
+		<div class="gardener-board row">
+			<div class="col-12 col-md-7">
+				<gardener-calendar></gardener-calendar>
+			</div>
+			<div class="col-12 col-md-5">
+				<div class="row">
+					<maintenance-card :garden="garden" :maintenance="maintenance"></maintenance-card>
 				</div>
-				<div class="col-12 col-md-5">
-					<div class="row">
-						<maintenance-card :garden="garden" :maintenance="maintenance"></maintenance-card>
-					</div>
-					<div class="row">
-						<meteo :garden="garden"></meteo>
-					</div>
-					<div>
-						<gardens-in-need></gardens-in-need>
-					</div>
+				<div class="row">
+					<meteo :garden="garden"></meteo>
+				</div>
+				<div>
+					<gardens-in-need></gardens-in-need>
 				</div>
 			</div>
 		</div>
--- code/public/js/router.js
+++ code/public/js/router.js
@@ -3,8 +3,10 @@ const DBURL = "http://localhost:3000/api"
 const router = new VueRouter({
     mode: 'history',
     routes: [
+      { path: '/gardener-board/:id', name: 'Gardener', component: GardenerBoard },
       { path: '/404', component: NotFound },  
       { path: '*', redirect: '/404' }
     ]
 })
 
+  
