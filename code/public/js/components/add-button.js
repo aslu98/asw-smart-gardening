a87d29e2f7a691839101f00be36bef78cb51e57d
Asia <asia.lucchi@studio.unibo.it>
2021-06-21 15:31:16 +0200

    modal working

--- code/public/js/components/add-button.js
+++ code/public/js/components/add-button.js
@@ -1,10 +1,9 @@
 const AddButton = {
     template: `
-
         <div class="col-2 vcenter-item">
             <button type="button" class="btn btn-success py-0 px-1 add-btn" data-bs-toggle="modal" data-bs-target="#addMaint"> + </button>
    
-        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal fade" id="addMaint" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
           <div class="modal-dialog">
             <div class="modal-content">
               <div class="modal-header">
@@ -12,15 +11,23 @@ const AddButton = {
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
               </div>
               <div class="modal-body">
-                
+                ciao
               </div>
               <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Chiudi</button>
-                <button type="button" class="btn btn-primary"> Salva</button>
+                <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Chiudi</button>
+                <button type="button" class="btn btn-success"> Salva</button>
               </div>
             </div>
           </div>
         </div>
     </div>
-    `
+    `,
+    /*mounted(){
+        /*var myModal = document.getElementById('addMaint')
+        var myInput = document.getElementById('myInput')
+
+        myModal.addEventListener('shown.bs.modal', function () {
+            myInput.focus()
+        })+/
+    }*/
 }
