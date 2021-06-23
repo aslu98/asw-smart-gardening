8c1d86e64f88973018e6f78902fc9b55d8be9745
Andrea Negri <andrea.negri4@studio.unibo.it>
2021-06-23 16:54:46 +0200

    Revert "Added Home button in the nav"
    This reverts commit ba4d83eff17ad8e2b424c41df0ea9e8661ca11f8.

--- code/public/js/navbar.js
+++ code/public/js/navbar.js
@@ -1,16 +1,14 @@
 const Navbar = {
     template: `
         <nav class="navbar px-2">
-            <div id="logo-container" class="col-xxl-7 col-lg-5 col-sm-12 col-12 d-flex align-items-center">
-                <input type="button" @click="openMap" value="Home" class="btn btn-success rounded-pill mx-4">
-<!--                <i class="fas fa-home fa-3x px-4" @click="openMap" style="cursor: pointer;"></i>-->
+            <div id="logo-container" class="col-xxl-7 col-lg-4 col-sm-12 col-12">
                 <a href="/" id="main-title">
                     <img src="/static/img/GardenCareSmall.png" alt="GardenCare Logo">
                 </a>
             </div>
-            <form v-if="!token" id="form-login" method="post" @submit.prevent="login" class="col-xxl-5 col-lg-7 col-sm-12 col-12">
+            <form v-if="!token" id="form-login" method="post" @submit.prevent="login" class="col-xxl-5 col-lg-8 col-sm-12 col-12">
                 <div class="row mx-0 align-items-center">
-                    <div class="col col-7">
+                    <div class="col col-xl-8 col-7">
                         <div class="row mx-0">
                             <div class="col input-group input-group-sm ps-0">
                                 <input id="userLogin" v-model="usernameLogin" class="form-control" :class="{ 'is-invalid': loginError }"
@@ -23,16 +21,22 @@ const Navbar = {
                         </div>
                         <div v-if="loginError" class="login-alert-error text-center mt-1"> {{ loginError }} </div>
                     </div>                    
-                    <div id="form-button-container" class="col col-5">
+                    <div id="form-button-container" class="col col-xl-4 col-5">
                         <input type="submit" value="Login" class="btn btn-success rounded-pill"/>
                         <input type="button" @click="openRegistrationForm" value="Registrati" class="btn btn-success rounded-pill"/>
                     </div>
                 </div>
             </form>
-            <div v-else class="row col-xxl-5 col-lg-7 col-sm-12 col-12 d-flex justify-content-end mx-0">
+            <div v-else class="row col-xxl-5 col-lg-8 col-sm-12 col-12 d-flex justify-content-around mx-0">
+<!--                <div class="col-3 text-center">
+                  
+                </div>-->
                 <div class="col-3 text-center">
-                    <input type="button" @click="openBoard" value="Manutenzioni" class="btn btn-success rounded-pill">
-                </div> 
+                    <input type="button" @click="openBoard" value="Board personale" class="btn btn-success rounded-pill">
+                </div>
+                <div class="col-3 text-center">
+                    <input type="button" value="Le mio infomazioni" class="btn btn-success rounded-pill">
+                </div>
                 <div class="col-3 text-center">
                     <input type="button" value="Logout" @click="logout" class="btn btn-danger rounded-pill">
                 </div>
@@ -94,9 +98,6 @@ const Navbar = {
         },
         openRegistrationForm() {
             this.$router.replace('/registration-form').catch(err => {});
-        },
-        openMap() {
-            this.$router.replace('/').catch(err => {});
         }
     },
     mounted() {
